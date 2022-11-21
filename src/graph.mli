ef2421057b0827d7312117f5f6c2f9d1a582be2f
Joelimgu <joelimgu@gmail.com>
2022-11-21 16:52:01 +0100

    clone_nodes

--- src/graph.mli
+++ src/graph.mli
@@ -19,7 +19,7 @@ val new_node: 'a graph -> id -> 'a graph
 
 (* new_arc gr id1 id2 lbl  : adds an arc from node id1 to node id2 with label lbl
  * Both nodes must already exist in the graph.
- * If the arc already exists, its label is replaced by lbl. 
+ * If the arc already exists, its label is replaced by lbl.
  * @raise Graph_error if node id1 or id2 does not exist in the graph. *)
 val new_arc: 'a graph -> id -> id -> 'a -> 'a graph
 
@@ -29,7 +29,7 @@ val new_arc: 'a graph -> id -> id -> 'a -> 'a graph
 (* node_exists gr id  indicates if the node with identifier id exists in graph gr. *)
 val node_exists: 'a graph -> id -> bool
 
-(* Type of lists of outgoing arcs of a node. 
+(* Type of lists of outgoing arcs of a node.
  * An arc is represented by a pair of the destination identifier and the arc label. *)
 type 'a out_arcs = (id * 'a) list
 
@@ -37,7 +37,7 @@ type 'a out_arcs = (id * 'a) list
  * @raise Graph_error if the id is unknown in the graph. *)
 val out_arcs: 'a graph -> id -> 'a out_arcs
 
-(* find_arc gr id1 id2  finds an arc between id1 and id2 and returns its label. Returns None if the arc does not exist. 
+(* find_arc gr id1 id2  finds an arc between id1 and id2 and returns its label. Returns None if the arc does not exist.
  * @raise Graph_error if id1 is unknown. *)
 val find_arc: 'a graph -> id -> id -> 'a option
 
--- src/tools.ml
+++ src/tools.ml
@@ -1,11 +1,13 @@
 open Graph
 
-let clone_nodes gr = assert false
+let clone_nodes gr = n_fold gr (fun gr2 n -> new_node gr2 n) empty_graph
+;;
+
 
 let gmap gr f = assert false
 
 (* TODO : TRY TO FIND A WAY TO GET THE WEIGHT OF THE ARC IN ORDER TO REPLACE IT IN CASE IF ARC EXISTS *)
-let add_arc gr n1 n2 weight = 
+let add_arc gr n1 n2 weight =
   match (find_arc gr n1 n2) with
   | Some(x) -> new_arc gr n1 n2 (weight + x)
   | None -> new_arc gr n1 n2 weight
