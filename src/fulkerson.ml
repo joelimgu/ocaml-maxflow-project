285ad7fdcc3a509103285bfd35c182ad93a1066d
joel <joelimgu@gmail.com>
2022-12-02 16:41:43 +0100

    it compiles doesn't work

--- src/fulkerson.ml
+++ src/fulkerson.ml
@@ -23,35 +23,42 @@ let fulkerson graph start dest =
     let cap_graph = gmap graph (function | (poids,id) -> ((0,poids), id)) in
     (* find arcs in a path *)
     let rec get_arcs g path acc = match path with
-    | a::b::rest -> function | Some(x) -> (find_arc g a b)::acc | None -> acc
+    | a::b::rest -> (function | Some(x) ->  x::acc | None -> acc) (find_arc g a b)
     | _ -> acc
     in
     (* replace arcs in the path with the new arcs in the list*)
-    let rec map_path g path arc_list = match (path,arc_list) with
-    | (a::b::rest_path,(l,id)::rest_arc) -> new_arc (map_path g rest_path rest_arc) a b l
+    let rec map_path g path new_arc_list = match (path,new_arc_list) with
+    | (a::b::rest_path,(l,_)::rest_arc) -> new_arc (map_path g rest_path rest_arc) a b l
     | _ -> g
     in
     (* get the diff between poids and cap of an arc *)
     let get_diff arc = match arc with
-    | ((cap, poids),_) -> poids-cap
+    | ((cap, poids),_) -> poids - cap
     in
-    (*get min betwwen a number and the arc diff *)
+    (*get min between a number and the arc diff *)
     let min_arc min_acc arc = if (get_diff arc) < min_acc then get_diff arc else min_acc
     in
-    let in_fulkerson n acc =
-        let path = match dfs graph start dest with
-           | None -> acc
+    (*TODO return capacity at some point...*)
+    let rec in_fulkerson n acc =
+        let path = match (dfs graph start dest) with
+           | None -> []
            | Some(path) -> path
         in
-        let arcs_in_path = get_arcs graph path []
-        in
-        let min = List.fold_left (min_arc) (List.hd arcs_in_path) arcs_in_path
-        in
-        let new_arcs = List.map (function | ((cap, poids),id) -> ((cap+min,poids), id)) arcs_in_path
-        in
-        ()
+        match path with
+        | [] -> acc
+        | path ->
+            let arcs_in_path = get_arcs graph path []
+            in
+            (* value to add to every arc *)
+            let min = List.fold_left (min_arc) (get_diff (List.hd arcs_in_path)) arcs_in_path
+            in
+            let new_arcs = List.map (function | ((cap, poids),id) -> ((cap+min,poids), id)) arcs_in_path
+            in
+            let updated_graph = map_path acc path new_arcs
+            in
+            in_fulkerson updated_graph
     in
-    ()
+    in_fulkerson 0 graph
 ;;
 
 (*(int*(int*id)) <=> (int*int*id)*)
